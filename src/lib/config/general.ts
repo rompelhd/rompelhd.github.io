c13060dc43952bbe19bb19291f049feaa52da1ff
rompelhd <75935831+rompelhd@users.noreply.github.com>
2022-12-03 19:34:34 +0100

    Add files via upload

---
+++ src/lib/config/general.ts
@@ -0,0 +1,160 @@
+import type { ThemeConfig, HeadConfig, HeaderConfig, FooterConfig, DateConfig, FeedConfig } from '$lib/types/general'
+
+export const theme: ThemeConfig = [
+  {
+    name: 'light',
+    text: '🌕 Claro'
+  },
+  {
+    name: 'dark',
+    text: '🌑 Oscuro'
+  },
+  {
+    name: 'cupcake',
+    text: '🧁 Cupcake'
+  },
+  {
+    name: 'bumblebee',
+    text: '🐝 Bumblebee'
+  },
+  {
+    name: 'emerald',
+    text: '✳️ Emerald'
+  },
+  {
+    name: 'corporate',
+    text: '🏢 Corporate'
+  },
+  {
+    name: 'valentine',
+    text: '🌸 Valentine'
+  },
+  {
+    name: 'synthwave',
+    text: '🌃 Synthwave'
+  },
+  {
+    name: 'retro',
+    text: '🌇 Retro'
+  },
+  {
+    name: 'cyberpunk',
+    text: '🌐 Cyberpunk'
+  },
+  {
+    name: 'halloween',
+    text: '🎃 Halloween'
+  },
+  {
+    name: 'garden',
+    text: '🏡 Garden'
+  },
+  {
+    name: 'forest',
+    text: '🌲 Forest'
+  },
+  {
+    name: 'aqua',
+    text: '💦 Aqua'
+  },
+  {
+    name: 'lofi',
+    text: '🎶 Lo-Fi'
+  },
+  {
+    name: 'pastel',
+    text: '🌈 Pastel'
+  },
+  {
+    name: 'fantasy',
+    text: '🐣 Fantasy'
+  },
+  {
+    name: 'wirefream',
+    text: '📱 Wireframe'
+  },
+  {
+    name: 'black',
+    text: '🖤 Black'
+  },
+  {
+    name: 'luxury',
+    text: '💰 Luxury'
+  },
+  {
+    name: 'dracula',
+    text: '🧛 Dracula'
+  },
+  {
+    name: 'cmyk',
+    text: '🖨️ CMYK'
+  },
+  {
+    name: 'autumn',
+    text: '🍂 Autumn'
+  },
+  {
+    name: 'business',
+    text: '🗄️ Business'
+  },
+  {
+    name: 'acid',
+    text: '🌧️ Acid'
+  },
+  {
+    name: 'lemonade',
+    text: '🍋 Lemonade'
+  },
+  {
+    name: 'night',
+    text: '🌃 Night'
+  },
+  {
+    name: 'coffee',
+    text: '☕ Coffee'
+  },
+  {
+    name: 'winter',
+    text: '❄️ Winter'
+  }
+]
+
+export const head: HeadConfig = {}
+
+export const header: HeaderConfig = {
+  nav: [
+/*    {
+      text: 'Bienvenidos',
+      link: '/hello-world'
+    },
+    {
+      text: 'Normas',
+      link: '/hello-world/elements'
+    }*/
+  ]
+}
+
+export const footer: FooterConfig = {
+  nav: [
+/*    {
+      text: '',
+      link: ''
+    },
+    {
+      text: '',
+      link: ''
+    }*/
+  ]
+}
+
+export const date: DateConfig = {
+  locales: 'es-ES',
+  options: {
+    year: '2-digit',
+    weekday: 'long',
+    month: 'short',
+    day: 'numeric'
+  }
+}
+
+export const feed: FeedConfig = {}
