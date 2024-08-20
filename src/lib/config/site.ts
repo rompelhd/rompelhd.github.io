c33a1fb43af76c92506d86799a89a77b32609bac
Rompelhd <75935831+rompelhd@users.noreply.github.com>
2024-08-20 20:50:12 +0200

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,14 +2,14 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'urara-demo.netlify.app',
-  title: 'Urara',
-  subtitle: 'Sweet & Powerful SvelteKit Blog Template',
-  lang: 'en-US',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'bl4ckh4ck.github.io',
+  title: 'Bl4ckH4ck',
+  subtitle: 'Tech Blog on Cybersecurity and Programming Insights',
+  lang: 'es-ES',
   description: 'Powered by SvelteKit/Urara',
   author: {
     avatar: '/assets/maskable@512.png',
-    name: 'John Doe',
+    name: 'Rompelhd',
     status: '🌸',
     bio: 'lorem ipsum dolor sit amet, consectetur adipiscing elit.'
   },
